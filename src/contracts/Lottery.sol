0e57af633b86991f266ac069f343a32f37304fa4
Cloudless67 <cloudlesslakitu67@gmail.com>
2021-12-10 00:05:49 +0900

    (WIP) Writing lottery contract

---
+++ src/contracts/Lottery.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract Lottery {
+    AggregatorV3Interface internal eth2usdPriceFeed;
+
+    uint256 public lotteryPrice;
+    address payable[] public participants;
+
+    constructor(uint256 _lotteryPrice) {
+        lotteryPrice = _lotteryPrice;
+        eth2usdPriceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+    }
+
+    function participate(uint8[] memory numbers) public payable {
+        //
+    }
+}
