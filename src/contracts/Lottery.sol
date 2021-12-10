2bd354025ba5fb53cfb554dc983fed982ab86a56
Cloudless67 <cloudlesslakitu67@gmail.com>
2021-12-10 18:47:11 +0900

    Added custom lottery token

--- src/contracts/Lottery.sol
+++ src/contracts/Lottery.sol
@@ -13,7 +13,7 @@ contract Lottery {
     constructor(uint256 _lotteryPrice) {
         lotteryPrice = _lotteryPrice;
         eth2usdPriceFeed = AggregatorV3Interface(
-            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e  // Rinkeby
         );
     }
 
