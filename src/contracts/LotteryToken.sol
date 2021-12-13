13c2858de0ae3860a0d7b668d7e75a035ad330cb
Cloudless67 <cloudlesslakitu67@gmail.com>
2021-12-13 11:20:53 +0900

    Added python import path

--- src/contracts/LotteryToken.sol
+++ src/contracts/LotteryToken.sol
@@ -1,10 +1,9 @@
-// contracts/GLDToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
+contract CloudlessLotteryToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("CloudlessLotteryToken", "CLT") {
         _mint(msg.sender, initialSupply);
     }
